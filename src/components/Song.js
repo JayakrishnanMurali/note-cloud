e34ea77a090fdf0e606016e63f5ee350c9b2eba2
Jayakrishnan <jayakrishnan1012@gmail.com>
2022-05-30 18:52:33 +0530

    Add Background image

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,11 +2,20 @@ import React from "react";
 
 const Song = ({ currentSong }) => {
   return (
-    <div className="song-container">
-      <img alt={currentSong.name} src={currentSong.cover}></img>
-      <h2>{currentSong.name}</h2>
-      <h3>{currentSong.artist}</h3>
-    </div>
+    <>
+      <div className="bg-fade"></div>
+      <div
+        className="bg-container"
+        style={{
+          backgroundImage: `url(${currentSong.cover})`,
+        }}
+      ></div>
+      <div className="song-container">
+        <img alt={currentSong.name} src={currentSong.cover}></img>
+        <h2>{currentSong.name}</h2>
+        <h3>{currentSong.artist}</h3>
+      </div>
+    </>
   );
 };
 
